2fdb7d7e558bf2f98604450926ad4669d2bb4767
Bell Coutinho <bell.sccoutinho@gmail.com>
2020-10-14 18:49:32 +0000

    Alterando o main

--- lab7/src/main/Main.java
+++ lab7/src/main/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         SanduicheFactory factory = new SanduicheFactory();
 
+        System.out.println("Sanduiches:");
         Sanduiche s1 = factory.getSanduiche(SanduicheTipo.INTEGRAL);
         System.out.println(s1.toString());
 
